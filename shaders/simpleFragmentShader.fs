5246ddcb73c5b051966e60983f875e92288d6eb9
jakobharlan <jakob.harlan@uni-weimar.de>
2014-11-13 21:14:49 +0100

    cell-shading

--- shaders/simpleFragmentShader.fs
+++ shaders/simpleFragmentShader.fs
@@ -4,7 +4,7 @@ float distance_func(vec3 vector_1, vec3 vector_2);
 
 uniform vec3 PlanetColor;
 uniform mat4 ViewMatrix;
-uniform vec3 LightPosition;
+uniform int ShadingMode; // 1: Normal Phong Shading   2: Cell Shading
 
 in  vec4 passed_normal;
 in  vec3 passed_vs_position;
@@ -12,10 +12,8 @@ in  vec3 passed_vs_position;
 out vec4 out_Color;
 
 float ka = 0.2f;
-float sun_kd = 0.5f;
-float sun_ks = 1.0f;
-float light_kd = 0.1f;
-float light_ks = 0.5f;
+float kd = 0.5f;
+float ks = 1.0f;
 int n = 15;
 
 
@@ -23,33 +21,42 @@ void main(void)
 {
  	vec3 vec_to_fragment = normalize(passed_vs_position);
 
-    // if (abs(dot(passed_normal.xyz, (-1.0)*vec_to_fragment ) ) < 0.4)
-    // {
-    // 	out_Color = vec4(0.0, 0.0, 0.0, 1.0);
-    // }
-    // else
-    {
-        float ambient  = ka;
-
-        vec4 sun_location = ViewMatrix*vec4(0.0, 0.0, 0.0, 1.0);
+ 	if (ShadingMode == 1)
+ 	{
+    	vec4 sun_location = ViewMatrix*vec4(0.0, 0.0, 0.0, 1.0);
     	vec3 vertex_to_sun = normalize(sun_location.xyz-passed_vs_position);
     	float dot_product_diffus = dot(passed_normal.xyz, vertex_to_sun);
     	float dot_product_specular = dot(normalize(reflect(vertex_to_sun, passed_normal.xyz)), normalize(passed_vs_position)); 
-    	float diffus   = sun_kd * max(0.0, dot_product_diffus);
-    	float specular = sun_ks * max(0.0, dot_product_specular);
-        float total_sun = distance_func(sun_location.xyz, passed_vs_position) * (diffus + pow(specular, n));
-
-        vec4 light_location = ViewMatrix*vec4(LightPosition, 1.0);
-        vec3 vertex_to_light = normalize(light_location.xyz-passed_vs_position);
-        dot_product_diffus = dot(passed_normal.xyz, vertex_to_light);
-        dot_product_specular = dot(normalize(reflect(vertex_to_light, passed_normal.xyz)), normalize(passed_vs_position)); 
-        diffus   = light_kd * max(0.0, dot_product_diffus);
-        specular = light_ks * max(0.0, dot_product_specular);
-        float total_lightsource = distance_func(light_location.xyz, passed_vs_position) * (diffus + pow(specular, n));
-
-    	vec3 total    = PlanetColor * ( ambient + total_sun + total_lightsource);
-        out_Color     = vec4(total, 1.0);
-    	// out_Color = vec4(abs(passed_normal.x), abs(passed_normal.y), abs(passed_normal.z), 1.0);
+    	vec3 ambient  = ka * PlanetColor;
+    	float diffus   = kd * max(0.0, dot_product_diffus);
+    	float specular = ks * max(0.0, dot_product_specular);
+    	vec3 total    = ambient + PlanetColor * distance_func(sun_location.xyz, passed_vs_position) * (diffus + pow(specular, n));
+    	out_Color     = vec4(total, 1.0);
+	}
+    if (ShadingMode == 2)
+    {
+	    if (abs(dot(passed_normal.xyz, (-1.0)*vec_to_fragment ) ) < 0.3)
+	    {
+	    	out_Color = vec4(0.0, 0.0, 0.0, 1.0);
+	    }
+	    else
+	    {
+    		vec4 sun_location = ViewMatrix*vec4(0.0, 0.0, 0.0, 1.0);
+    		vec3 vertex_to_sun = normalize(sun_location.xyz-passed_vs_position);
+    		float dot_product = dot(passed_normal.xyz, vertex_to_sun);
+    		if (dot_product < 0.3)
+    		{
+    			out_Color = vec4(0.3 * PlanetColor, 1.0);
+    		}
+    		else if (dot_product < 0.7)
+    		{
+				out_Color = vec4(0.6 * PlanetColor, 1.0);
+    		}
+    		else
+    		{
+				out_Color = vec4(PlanetColor, 1.0);
+    		}
+	    }
     }
 
 }
